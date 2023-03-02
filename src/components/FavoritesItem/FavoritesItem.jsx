ee02ad66fcddff4f592e5e8a1df1891ec2f4ba9e
Andrea Love <andrealovelovelove@gmail.com>
2023-03-02 14:48:02 -0600

    added functions and return to FavoritesItem

--- src/components/FavoritesItem/FavoritesItem.jsx
+++ src/components/FavoritesItem/FavoritesItem.jsx
@@ -1,10 +1,40 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
 
 
 function FavoritesItem() {
+    const dispatch = useDispatch(); 
+    const [categoryVariable, setCategoryVariable] = useState(''); 
+
+    const handleClick = () => {
+          //this is where the put dispatch will be called 
+        // dispatch({
+            // type: 'CHANGE_CATEGORY', 
+            // payload: categoryVariable
+        // })
+    }
+
+    //need to get the list of categories to map over 
+    const getCategories = () => {
+        dispatch({type: 'GET_CATEGORIES'})
+    }
+
     return(
+     
+        //map over category array to render individual options 
+
         <div>
-            {/* space for photo url */}
+            {/* <img src="url will go here"/> */}
+            <select onChange={(event) => setCategoryVariable({idOfPhoto: id, category: event.target.value})}>
+                {/* change these later to the variables */}
+                <option value="variablefunny">funny</option>
+                <option value="variablecohort">cohort</option>
+                <option value="variablecartoon">cartoon</option>
+                <option value="variablensfw">nsfw</option>
+                <option value="variablememe">meme</option>
+            </select>
+            <button onClick={handleClick}></button>
         </div>
     )  
 
--- src/components/FavoritesList/FavoritesList.jsx
+++ src/components/FavoritesList/FavoritesList.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { useSelector} from 'react-redux';
 
 function FavoritesList() {
+                                                //need to change this to the reducer name 
+    // const listOfPhotos = useSelector(store => store.reducer)
+
+    
+
+
     return{  
-        
+
     }
 
 }
