f217c1fb003b735641cb06ee5f53104baa4e4f73
Ben Capp <bentucker333@gmail.com>
2023-03-02 13:55:27 -0600

    created search item and search list components

--- src/components/SearchList/SearchList.jsx
+++ src/components/SearchList/SearchList.jsx
@@ -2,8 +2,14 @@ import SearchItem from "../SearchItem/SearchItem";
 import { useSelector } from "react-redux";
 
 function SearchList() {
-  const gifList = useSelector((store) => store.list);
-  return <div id="search-list">{}</div>;
+  const gifList = []; //TODO: useSelector((store) => store.list);
+  return (
+    <div id="search-list">
+      {gifList.map((element) => (
+        <SearchItem key={element.id} gif={element} />
+      ))}
+    </div>
+  );
 }
 
 export default SearchList;
