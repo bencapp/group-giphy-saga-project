f3bd1ed05e8d4e3fa33df9d7126e27de3208282f
Ben Capp <bentucker333@gmail.com>
2023-03-02 13:12:37 -0600

    updated database.sql

--- database.sql
+++ database.sql
@@ -12,3 +12,11 @@ CREATE TABLE "category" (
 -- Default categories. You may change them :)
 INSERT INTO "category" ("name")
 VALUES ('funny'), ('cohort'), ('cartoon'), ('nsfw'), ('meme');
+
+-- Favorites table
+CREATE TABLE "favorites" (
+	"id" SERIAL PRIMARY KEY,
+	"url" VARCHAR (500),
+	"category_id" INT
+);
+
